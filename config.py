e3a303659c3635b29cd55579ddebeb07b02b06ea
Gevorg <gevman97@gmail.com>
2019-11-07 16:30:52 +0400

    app

--- config.py
+++ config.py
@@ -2,10 +2,11 @@ class Configuration(object):
     DEBUG=True
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     SQLALCHEMY_ECHO = True
-   # CLEARDB_DATABASE_URL='mysql+mysqlconnector://root:''@localhost/corporate'
-    CLEARDB_DATABASE_URL = 'mysql://b49680054be872:18a9dcb2@us-cdbr-iron-east-05.cleardb.net/heroku_a28949ef640e9b2?reconnect=true'
+    #SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://root:''@localhost/corporate'
+    SQLALCHEMY_DATABASE_URI='mysql://b49680054be872:18a9dcb2@us-cdbr-iron-east-05.cleardb.net/heroku_a28949ef640e9b2?reconnect=true/corporate'
+    #CLEARDB_DATABASE_URL = 'mysql://b49680054be872:18a9dcb2@us-cdbr-iron-east-05.cleardb.net/heroku_a28949ef640e9b2?reconnect=true'
 
-    MYSQL_HOST = 'us-cdbr-iron-east-05.cleardb.net'
-    MYSQL_USER = 'b49680054be872'
-    MYSQL_PASSWORD = '18a9dcb2'
-    MYSQL_DB = 'heroku_a28949ef640e9b2'
+    #MYSQL_HOST = 'us-cdbr-iron-east-05.cleardb.net'
+    #MYSQL_USER = 'b49680054be872'
+    #MYSQL_PASSWORD = '18a9dcb2'
+    #MYSQL_DB = 'heroku_a28949ef640e9b2'
