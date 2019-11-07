e6bb92e3ab964334aed4b99b28197aed5a6fd967
Gevorg <gevman97@gmail.com>
2019-11-07 17:53:34 +0400

    app

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
+import os
 class Configuration(object):
     DEBUG=True
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://root:''@localhost/corporate'
+    #SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://root:''@localhost/corporate'
+    SQLALCHEMY_DATABASE_URI=os.environ('CLEARDB_DATABASE_URL')
     #mysql://b49680054be872:18a9dcb2@us-cdbr-iron-east-05.cleardb.net/heroku_a28949ef640e9b2?reconnect=true
     #SQLALCHEMY_DATABASE_URI='mysql://b49680054be872:18a9dcb2@us-cdbr-iron-east-05.cleardb.net/heroku_a28949ef640e9b2?reconnect=true/corporate'
     #CLEARDB_DATABASE_URL = 'mysql://b49680054be872:18a9dcb2@us-cdbr-iron-east-05.cleardb.net/heroku_a28949ef640e9b2?reconnect=true'
