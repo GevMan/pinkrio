3a7c923160966690f199ca14caba5b6ce3b030a8
Gevorg <gevman97@gmail.com>
2019-11-07 12:41:59 +0400

    app

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ class Configuration(object):
     DEBUG=True
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://root:''@localhost/corporate'
+    JAWSDB_URL='mysql+mysqlconnector://root:''@localhost/corporate'
