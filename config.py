eab7030fba0ae4facba151763327c8d9953da8f9
Gevorg <gevman97@gmail.com>
2019-11-07 17:21:49 +0400

    app

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ class Configuration(object):
     DEBUG=True
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     SQLALCHEMY_ECHO = True
-    #SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://root:''@localhost/corporate'
-    SQLALCHEMY_DATABASE_URI='mysql://b49680054be872:18a9dcb2@us-cdbr-iron-east-05.cleardb.net/heroku_a28949ef640e9b2?reconnect=true/corporate'
+    SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://root:''@localhost/corporate'
+    #SQLALCHEMY_DATABASE_URI='mysql://b49680054be872:18a9dcb2@us-cdbr-iron-east-05.cleardb.net/heroku_a28949ef640e9b2?reconnect=true/corporate'
     #CLEARDB_DATABASE_URL = 'mysql://b49680054be872:18a9dcb2@us-cdbr-iron-east-05.cleardb.net/heroku_a28949ef640e9b2?reconnect=true'
 
     #MYSQL_HOST = 'us-cdbr-iron-east-05.cleardb.net'
