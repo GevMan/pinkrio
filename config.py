56857f233fdd4382bcce78007ce434c1dca66340
Gevorg <gevman97@gmail.com>
2019-11-07 15:29:14 +0400

    app

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ class Configuration(object):
     DEBUG=True
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     SQLALCHEMY_ECHO = True
-    CLEARDB_DATABASE_URL='mysql+mysqlconnector://root:''@localhost/corporate'
+   # CLEARDB_DATABASE_URL='mysql+mysqlconnector://root:''@localhost/corporate'
+    CLEARDB_DATABASE_URL = 'mysql://b49680054be872:18a9dcb2@us-cdbr-iron-east-05.cleardb.net/heroku_a28949ef640e9b2?reconnect=true'
