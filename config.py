901fb1a021e14b8e9dcc50a0f0c9d53e950b4e56
Gevorg <gevman97@gmail.com>
2019-11-07 13:34:58 +0400

    app

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ class Configuration(object):
     DEBUG=True
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     SQLALCHEMY_ECHO = True
-    JAWSDB_URL='mysql+mysqlconnector://root:''@localhost/corporate'
+    CLEARDB_DATABASE_URL='mysql+mysqlconnector://root:''@localhost/corporate'
